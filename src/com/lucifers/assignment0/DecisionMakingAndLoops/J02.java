f0fda966d0799c42c0d9581d60f937c703331cf3
Himanshu Singh <hs098605@gmail.com>
2021-03-28 01:21:06 +0530

    Program to Display Alphabets (A to Z) using loop.

---
+++ src/com/lucifers/assignment0/DecisionMakingAndLoops/J02.java
@@ -0,0 +1,10 @@
+//Program to Display Alphabets (A to Z) using loop.
+package com.lucifers.assignment0.DecisionMakingAndLoops;
+
+public class J02 {
+    public static void main(String[] args) {
+        System.out.print("Alphabets from A to Z are : ");
+        for(int i = 65; i <= 90; i++)
+            System.out.print((char)i +" ");
+    }
+}
